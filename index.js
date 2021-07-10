b0cd73763f37babd407832c3b14db7b81d02d9d8
imamhossain94 <imamagun94@gmail.com>
2021-07-10 20:54:18 +0600

    feat: express app initiaized

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+//set up express app
+const app = express();
+
+
+//listen for requests
+app.listen(process.env.port || 4000, function() {
+    console.log("App running");
+});
