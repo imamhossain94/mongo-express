f8eabe08f87d2ba6e1d6c32a320efae358a3db37
imamhossain94 <imamagun94@gmail.com>
2021-07-14 15:25:16 +0600

    feat: added code for removing a mongo object.

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,10 @@ router.put('/ninjas/:id', (req, res, next)=> {
 
 //delete a ninja from the db
 router.delete('/ninjas/:id', (req, res, next)=> {
-    res.send({type:'DELETE'});
+
+    Ninja.findByIdAndRemove({_id: req.params.id}).then((ninja)=>{
+        res.send(ninja);
+    });
 });
 
 module.exports = router;
