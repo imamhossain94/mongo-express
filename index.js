86d7b17764aa8ffea0c65169558dc184c108cb7d
imamhossain94 <imamagun94@gmail.com>
2021-07-14 12:58:07 +0600

    feat: app connected to mongodb

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 //set up express app
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');
+mongoose.Promise = global.Promise;
+
 //initializing routes
 app.use('/api', require('./routes/api'));
 
