213ee75ed522db0e3563684f00a253b0b9ab8afd
imamhossain94 <imamagun94@gmail.com>
2021-07-14 15:31:16 +0600

    feat: added code for updating a mongo object using put request.

--- routes/api.js
+++ routes/api.js
@@ -17,12 +17,15 @@ router.post('/ninjas', (req, res, next)=> {
 
 //update a ninja in the db
 router.put('/ninjas/:id', (req, res, next)=> {
-    res.send({type:'PUT'});
+    Ninja.findByIdAndUpdate({_id: req.params.id}, req.body).then((ninja)=>{
+        Ninja.findOne({_id: req.params.id}).then((ninja)=>{
+            res.send(ninja);
+        });
+    });
 });
 
 //delete a ninja from the db
 router.delete('/ninjas/:id', (req, res, next)=> {
-
     Ninja.findByIdAndRemove({_id: req.params.id}).then((ninja)=>{
         res.send(ninja);
     });
